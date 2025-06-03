fb3f3993efa40efc969269feef9d6e75d0ae851b
ByteGab <gabrielfreiresimon@gmail.com>
2025-06-03 16:00:10 +0200

    add functions

--- philo/philosopher.h
+++ philo/philosopher.h
@@ -6,7 +6,7 @@
 /*   By: gafreire <gafreire@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/02 19:38:00 by gafreire          #+#    #+#             */
-/*   Updated: 2025/06/03 13:11:46 by gafreire         ###   ########.fr       */
+/*   Updated: 2025/06/03 15:42:10 by gafreire         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,6 +14,25 @@
 # define PHILOSOPHER_H
 
 # include <pthread.h>
+# include <stdio.h>
+# include <unistd.h>
+
+typedef struct s_statistics
+{
+	unsigned int	nbr_philos;
+	unsigned int	nbr_eats;
+	// os argumentos
+	unsigned int	time_eat;
+	unsigned int	time_sleep;
+	unsigned int	time_die;
+	// comenzo do tempo
+	unsigned int	start_time;
+	// control do mutex
+	pthread_mutex_t	*forks;
+	pthread_mutex_t	*death;
+	// mirar si morreu os filosofos
+	int				is_dead;
+}					t_statistics;
 
 typedef struct s_philosopher
 {
@@ -23,28 +42,13 @@ typedef struct s_philosopher
 	pthread_mutex_t	*fork_left;
 	pthread_mutex_t	*fork_right;
 	// a ultima vez que comeu (en microsegundos)
-	unsigned int			last_eat;
+	unsigned int	last_eat;
 	// cantas veces comeu
 	unsigned int	count_eat;
 	// punteiro a outra estructura (cambiar o nome)
-	t_statistics *statistics;
+	t_statistics	*statistic;
 }					t_philosopher;
 
-typedef struct s_statistics
-{
-	unsigned int nbr_philos;
-	unsigned int nbr_eats;
-	// os argumentos
-	unsigned int time_eat;
-	unsigned int time_sleep;
-	unsigned int time_die;
-	// comenzo do tempo
-	unsigned int start_time;
-	// control do mutex
-	pthread_mutex_t *forks;
-	pthread_mutex_t *death;
-	// mirar si morreu os filosofos
-	int is_dead;
-}					t_statistics;
-
+void				simple_philo(char *philo,char *die);
+void				advanced_philo(void);
 #endif
