870b12b70a33931406aff906e02cd7c5ef4d16fe
ByteGab <gabrielfreiresimon@gmail.com>
2025-06-04 14:52:47 +0200

    change parse

--- philo/parse.c
+++ philo/parse.c
@@ -6,7 +6,7 @@
 /*   By: gafreire <gafreire@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/03 15:01:46 by gafreire          #+#    #+#             */
-/*   Updated: 2025/06/03 15:39:40 by gafreire         ###   ########.fr       */
+/*   Updated: 2025/06/04 13:48:08 by gafreire         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,7 +15,10 @@
 int	main(int argc, char *argv[])
 {
 	if (argc == 5)
+	{
+		
 		simple_philo(argv[2],argv[3]);
+	}
 	else if (argc == 6)
 		advanced_philo();
 	else
