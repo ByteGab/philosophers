3eee63c33ae9c9b52431614e022406d8a96cd841
ByteGab <gabrielfreiresimon@gmail.com>
2025-06-05 13:14:48 +0200

    add all variables

--- philo/philo.c
+++ philo/philo.c
@@ -6,13 +6,15 @@
 /*   By: gafreire <gafreire@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/03 15:02:43 by gafreire          #+#    #+#             */
-/*   Updated: 2025/06/04 13:04:57 by gafreire         ###   ########.fr       */
+/*   Updated: 2025/06/05 13:10:27 by gafreire         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "philosopher.h"
 
-void	simple_philo(char *philo, char *die)
+// cambiar segundos y micro a milisegundos
+void	simple_philo(unsigned int philo, unsigned int die, unsigned int eat,
+		unsigned int sleep)
 {
 	struct timeval	current_time;
 
@@ -20,6 +22,10 @@ void	simple_philo(char *philo, char *die)
 	printf("%s\n", "Cuatros argumentos");
 	printf("%s\n", "Cuatros argumentos");
 	printf("Number of philosophers\n");
+	printf("Philo: %d\n", philo);
+	printf("Die: %d\n", die);
+	printf("Eat: %d\n", eat);
+	printf("Sleep: %d\n", sleep);
 	gettimeofday(&current_time, NULL);
 	printf("segundos: %ld\nmicrosegundos: %ld\n", current_time.tv_sec,
 		current_time.tv_usec);
