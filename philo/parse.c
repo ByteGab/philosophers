c2748c43e0462db7045df580c9c01f60f62ca5b7
ByteGab <gabrielfreiresimon@gmail.com>
2025-06-05 13:13:51 +0200

    add all variables

--- philo/parse.c
+++ philo/parse.c
@@ -6,7 +6,7 @@
 /*   By: gafreire <gafreire@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/03 15:01:46 by gafreire          #+#    #+#             */
-/*   Updated: 2025/06/04 13:48:08 by gafreire         ###   ########.fr       */
+/*   Updated: 2025/06/05 13:05:21 by gafreire         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,8 +16,7 @@ int	main(int argc, char *argv[])
 {
 	if (argc == 5)
 	{
-		
-		simple_philo(argv[2],argv[3]);
+		simple_philo(ft_atoui(argv[1]),ft_atoui(argv[2]),ft_atoui(argv[3]),ft_atoui(argv[4]));
 	}
 	else if (argc == 6)
 		advanced_philo();
@@ -25,4 +24,3 @@ int	main(int argc, char *argv[])
 		printf("Error\n");
 	return (0);
 }
-// cambiar segundos y micro a milisegundos
