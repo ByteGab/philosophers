71c271ae4ff7e15e9ff120252205d82aeeb20131
ByteGab <gabrielfreiresimon@gmail.com>
2025-06-05 13:15:28 +0200

    complte ft_atoui

--- philo/utils.c
+++ philo/utils.c
@@ -6,11 +6,32 @@
 /*   By: gafreire <gafreire@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/04 13:48:28 by gafreire          #+#    #+#             */
-/*   Updated: 2025/06/04 13:51:56 by gafreire         ###   ########.fr       */
+/*   Updated: 2025/06/05 13:03:01 by gafreire         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-unsigned int ft_atoi(unsigned int )
+#include "philosopher.h"
+unsigned int	ft_atoui(char *str)
 {
-    
+	unsigned int nbr;
+	int i;
+
+	nbr = 0;
+	i = 0;
+
+	while (str[i] == ' ' || str[i] == '\t' || str[i] == '\r'
+		|| str[i] == '\n' || str[i] == '\f' || str[i] == '\v')
+		i++;
+    if (str[i] == '-')
+    {
+        printf("E numero negativo\n");
+		exit(1);
+        return (-1); // cambialo
+    }
+	while (str[i] >= '0' && str[i] <= '9')
+	{
+		nbr = nbr * 10 + str[i] - '0';
+		i++;
+	}
+	return (nbr);
 }
