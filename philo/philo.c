2ce8e4444a6a5bca2c15b9181662e1c7e4e8f834
ByteGab <gabrielfreiresimon@gmail.com>
2025-06-11 19:51:25 +0200

    add list

--- philo/philo.c
+++ philo/philo.c
@@ -6,7 +6,7 @@
 /*   By: gafreire <gafreire@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/03 15:02:43 by gafreire          #+#    #+#             */
-/*   Updated: 2025/06/09 17:16:26 by gafreire         ###   ########.fr       */
+/*   Updated: 2025/06/11 17:49:07 by gafreire         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,37 +16,58 @@
 void	simple_philo(unsigned int philo, unsigned int die, unsigned int eat,
 		unsigned int sleep)
 {
+	t_statistics	statistic;
+	t_philosopher	*philos;
+	
 	struct timeval	current_time;
-	if (philo == 0 || die  == 0 || eat == 0 || sleep == 0)
+	
+	// creo os datos
+	// t_philosopher philos;
+	if (philo == 0 || die == 0 || eat == 0 || sleep == 0)
 		printf("Os argumetos deberian ser maiores que cero\n");
-
-	// deber ter menos de cen numeros os filos?
-	// deber ter menos de doscentos numeros as demais variables?
+	statistic.nbr_philos = philo;
+	statistic.time_die = die; // ten que ser xa en milisegundos
+	statistic.time_eat = eat;
+	statistic.time_sleep = sleep;
+	// deber ter menos de cen numeros os filos? e mentira e na folla de correciones
+	// deber ter menos de doscentos numeros as demais variables? e mentira e na folla de correciones
 	usleep(2000000);
 	printf("%s\n", "Cuatros argumentos");
 	printf("Number of philosophers\n");
-	printf("Philo: %d\n", philo);
-	printf("Die: %d\n", die);
-	printf("Eat: %d\n", eat);
-	printf("Sleep: %d\n", sleep);
+	printf("Philo: %d\n", statistic.nbr_philos);
+	printf("Die: %d\n", statistic.time_die);
+	printf("Eat: %d\n", statistic.time_eat);
+	printf("Sleep: %d\n", statistic.time_sleep);
 	gettimeofday(&current_time, NULL);
 	printf("segundos: %ld\nmicrosegundos: %ld\n", current_time.tv_sec,
 		current_time.tv_usec);
+	philos = malloc(sizeof(t_philosopher) * )
 }
 void	advanced_philo(unsigned int philo, unsigned int die, unsigned int eat,
 		unsigned int sleep, unsigned int must_eat)
 {
+	// creo os datos
+	t_statistics statistic;
+	// t_philosopher philos;
+
 	struct timeval current_time;
-	if (philo == 0 || die  == 0 || eat == 0 || sleep == 0 || must_eat == 0)
+	if (philo == 0 || die == 0 || eat == 0 || sleep == 0 || must_eat == 0)
 		printf("Os argumetos deberian ser maiores que cero\n");
+
+	statistic.nbr_philos = philo;
+	statistic.time_die = die; // ten que ser xa en milisegundos
+	statistic.time_eat = eat;
+	statistic.time_sleep = sleep;
+	statistic.nbr_eats = must_eat;
+
 	usleep(2000000);
 	printf("%s\n", "Cinco argumentos");
 	printf("Number of philosophers\n");
-	printf("Philo: %d\n", philo);
-	printf("Die: %d\n", die);
-	printf("Eat: %d\n", eat);
-	printf("Sleep: %d\n", sleep);
-	printf("Must eat: %d\n", must_eat);
+	printf("Philo: %d\n", statistic.nbr_philos);
+	printf("Die: %d\n", statistic.time_die);
+	printf("Eat: %d\n", statistic.time_eat);
+	printf("Sleep: %d\n", statistic.time_sleep);
+	printf("Must eat: %d\n", statistic.nbr_eats);
 	gettimeofday(&current_time, NULL);
 	printf("segundos: %ld\nmicrosegundos: %ld\n", current_time.tv_sec,
 		current_time.tv_usec);
