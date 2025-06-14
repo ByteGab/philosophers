857ebbb2996cb581704ce887ddd494658125846e
ByteGab <gabrielfreiresimon@gmail.com>
2025-06-14 16:38:41 +0200

    change init forks and philos

--- philo/philo.c
+++ philo/philo.c
@@ -6,11 +6,50 @@
 /*   By: gafreire <gafreire@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/03 15:02:43 by gafreire          #+#    #+#             */
-/*   Updated: 2025/06/13 17:30:48 by gafreire         ###   ########.fr       */
+/*   Updated: 2025/06/14 16:24:43 by gafreire         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "philosopher.h"
+
+void	start_forks(int nbr_philo, t_statistics *statistics)
+// punteiro para que asi o cambio no queda en local
+{
+	int i;
+
+	i = 0;
+
+	while (i < nbr_philo)
+	{
+		pthread_mutex_init(&statistics->forks[i], NULL);
+		i++;
+	}
+}
+
+void	start_philo(t_philosopher *philo, t_statistics *statistics)
+{
+	unsigned int i;
+
+	i = 0;
+	while (i < statistics->nbr_philos)
+	{
+		philo[i].id = i;
+		philo[i].fork_left = &statistics->forks[i];
+		philo[i].fork_right = &statistics->forks[(i + 1) % statistics->nbr_philos];
+			// volta circular
+		philo[i].count_eat = 0;
+		philo[i].last_eat = 0;
+		philo[i].statistic = statistics;
+		i++;
+	}
+	i = 0;
+	while (i < statistics->nbr_philos)
+	{
+		printf("Id %d, Fork Left %p , Fork Rigth %p , Eat %d, Last Eat %d\n",philo[i].id,philo[i].fork_left,philo[i].fork_right,philo[i].count_eat,philo[i].last_eat);
+		i++;
+	}
+	
+}
 
 // cambiar segundos y micro a milisegundos
 void	simple_philo(unsigned int philos, unsigned int die, unsigned int eat,
@@ -34,13 +73,13 @@ void	simple_philo(unsigned int philos, unsigned int die, unsigned int eat,
 	if (!philo)
 	{
 		// liberalo
-		return (1); // cambialo
+		//return (1); // cambialo
 	}
 	statistic.forks = malloc(philos * sizeof(pthread_mutex_t));
 	if (!statistic.forks)
 	{
 		// liberalo
-		return (1); // cambialo
+		//return (1); // cambialo
 	}
 }
 void	advanced_philo(unsigned int philos, unsigned int die, unsigned int eat,
@@ -62,34 +101,15 @@ void	advanced_philo(unsigned int philos, unsigned int die, unsigned int eat,
 	if (!philo)
 	{
 		// liberar todo
-		return (1); // cambialo
+		//return (1); // cambialo
 	}
 	statistic.forks = malloc(philos * sizeof(pthread_mutex_t));
 	if (!statistic.forks)
 	{
 		// liberalo
-		return (1); // cambialo
+		//return (1); // cambialo
 	}
 	start_forks(philos, &statistic);
+	start_philo(philo,&statistic);
 }
 
-void	start_forks(int nbr_philo, t_statistics *statistics)
-		// punteiro para que asi o cambio no queda en local
-{
-	int i;
-
-	i = 0;
-
-	while (i < nbr_philo)
-	{
-		pthread_mutex_init(&statistics->forks[i], NULL);
-		i++;
-	}
-}
-
-void	start_philo(t_philosopher *philo, t_statistics *statistics)
-{
-	int i;
-
-	i = 0;
-}
