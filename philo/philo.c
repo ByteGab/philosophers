dfe9ea84704b27499c8111841c56e00de055921e
ByteGab <gabrielfreiresimon@gmail.com>
2025-06-25 13:47:39 +0200

    move functions to init

--- philo/philo.c
+++ philo/philo.c
@@ -6,93 +6,19 @@
 /*   By: gafreire <gafreire@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/03 15:02:43 by gafreire          #+#    #+#             */
-/*   Updated: 2025/06/23 17:55:00 by gafreire         ###   ########.fr       */
+/*   Updated: 2025/06/25 12:51:04 by gafreire         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "philosopher.h"
 
-void	start_forks(int nbr_philo, t_statistics *statistics)
-// punteiro para que asi o cambio no queda en local
-{
-	int i;
-
-	i = 0;
-
-	while (i < nbr_philo)
-	{
-		// if (pthread_mutex_init(&statistics->forks[i], NULL != 0))
-		// {
-		// 	while (i >= 0)
-		// 	{
-		// 		pthread_mutex_destroy(&statistics->forks[i]);
-		// 		i--;
-		// 	}
-		// 	// free
-		// 	//return (NULL);
-		// 	return ;
-		//}
-		pthread_mutex_init(&statistics->forks[i], NULL);
-		i++;
-	}
-}
-
-void	start_philo(t_philosopher *philo, t_statistics *statistics)
-{
-	unsigned int	i;
-
-	i = 0;
-	while (i < statistics->nbr_philos)
-	{
-		philo[i].id = i;
-		philo[i].fork_left = &statistics->forks[i];
-		philo[i].fork_right = &statistics->forks[(i + 1)
-			% statistics->nbr_philos];
-		// volta circular
-		philo[i].count_eat = 0;
-		philo[i].last_eat = statistics->start_time;
-		philo[i].statistic = statistics;
-		i++;
-	}
-	i = 0;
-	while (i < statistics->nbr_philos)
-	{
-		printf("Id %d, Fork Left %p , Fork Rigth %p , Eat %d, Last Eat %lu\n",
-			philo[i].id, philo[i].fork_left, philo[i].fork_right,
-			philo[i].count_eat, philo[i].last_eat);
-		i++;
-	}
-}
-
-void	start_threads(t_philosopher *philo)
-{
-	unsigned int	i;
-
-	i = 0;
-	while (i < philo->statistic->nbr_philos)
-	{
-		pthread_create(&philo[i].thread, NULL, routine, &philo[i]);
-		i++;
-	}
-}
-
-unsigned int	get_time(void)
-{
-	struct timeval	current_time;
-	long			time;
-
-	time = 0;
-	gettimeofday(&current_time, NULL);
-	time = (current_time.tv_sec * 1000) + (current_time.tv_usec / 1000);
-	return (time);
-}
-// cambiar segundos y micro a milisegundos
 void	simple_philo(unsigned int philos, unsigned int die, unsigned int eat,
 		unsigned int sleep)
 {
 	t_statistics	statistic;
 	t_philosopher	*philo;
 	pthread_t		keeper_thread;
+	unsigned int	i;
 
 	// creo os datos
 	// t_philosopher philos;
@@ -124,13 +50,20 @@ void	simple_philo(unsigned int philos, unsigned int die, unsigned int eat,
 		// return (1); // cambialo
 	}
 	pthread_mutex_init(statistic.death, NULL);
+	statistic.nbr_eats = 0; // cambialo
 	start_forks(philos, &statistic);
-	start_philo(philo, &statistic);
 	statistic.start_time = get_time();
+	start_philo(philo, &statistic);
 	printf("%lu\n", statistic.start_time);
 	start_threads(philo);
 	pthread_create(&keeper_thread, NULL, keeper_function, philo);
 	pthread_join(keeper_thread, NULL);
+	i = 0;
+	while (i < statistic.nbr_philos)
+	{
+		pthread_join(philo[i].thread, NULL);
+		i++;
+	}
 }
 void	advanced_philo(unsigned int philos, unsigned int die, unsigned int eat,
 		unsigned int sleep, unsigned int must_eat)
