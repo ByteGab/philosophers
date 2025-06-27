c99c3881a3c523d7f75a9d9e80d212085e28919e
ByteGab <gabrielfreiresimon@gmail.com>
2025-06-27 21:05:02 +0200

    add cheack params

--- philo/main.c
+++ philo/main.c
@@ -6,21 +6,43 @@
 /*   By: gafreire <gafreire@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/25 12:08:48 by gafreire          #+#    #+#             */
-/*   Updated: 2025/06/25 12:09:31 by gafreire         ###   ########.fr       */
+/*   Updated: 2025/06/27 20:10:28 by gafreire         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "philosopher.h"
 
+int	check_params(int argc, char **argv)
+{
+	int	i;
+
+	i = 1;
+	while (i < argc)
+	{
+		if (ft_atoui(argv[i]) == 0)
+			return (printf("Argumento no valido: %s\n", argv[i]), 0);
+		i++;
+	}
+	return (1);
+}
+
 int	main(int argc, char *argv[])
 {
-	if (argc == 5)
-		simple_philo(ft_atoui(argv[1]), ft_atoui(argv[2]), ft_atoui(argv[3]),
-			ft_atoui(argv[4]));
-	else if (argc == 6) // no pode recibir mais de 4 argumentos unha funcion
-		advanced_philo(ft_atoui(argv[1]), ft_atoui(argv[2]), ft_atoui(argv[3]),
-			ft_atoui(argv[4]), ft_atoui(argv[5]));
+	t_statistics	statistic;
+
+	if (argc == 5 || argc == 6)
+	{
+		if (!check_params(argc, argv))
+			return (0);
+		statistic.nbr_philos = ft_atoui(argv[1]);
+		statistic.time_die = ft_atoui(argv[2]); // ten que ser xa en milisegundos ?
+		statistic.time_eat = ft_atoui(argv[3]);
+		statistic.time_sleep = ft_atoui(argv[4]);
+		simple_philo(statistic);
+		if (argc == 6)
+			advanced_philo(statistic,ft_atoui(argv[5]));
+	}
 	else
-		printf("Error\n");
+		return (printf("Error\n"), 0);
 	return (0);
 }
