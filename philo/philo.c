4af9b433d61b2cd81421591cbf1ec7e018d12437
ByteGab <gabrielfreiresimon@gmail.com>
2025-06-29 22:58:35 +0200

    add free

--- philo/philo.c
+++ philo/philo.c
@@ -12,46 +12,61 @@
 
 #include "philosopher.h"
 
-void	simple_philo(t_statistics statistic)
+void	free_resources(t_statistics *statistic, t_philosopher *philo)
+{
+	unsigned int i;
+
+	i = 0;
+
+	while (i < statistic->nbr_philos)
+	{
+		pthread_mutex_destroy(&statistic->forks[i]);
+		i++;
+	}
+	pthread_mutex_destroy(statistic->death);
+	free(statistic->forks);
+	free(statistic->death);
+	free(philo);
+	
+}
+
+int	init_resources(t_statistics *statistic, t_philosopher **philo)
+{
+	*philo = malloc(statistic->nbr_philos * sizeof(t_philosopher));
+	if (!*philo)
+		return (0);
+	//memset(philo, 0, statistic.nbr_philos * sizeof(t_philosopher));
+	statistic->forks = malloc(statistic->nbr_philos * sizeof(pthread_mutex_t));
+	if (!statistic->forks)
+	{
+		free(*philo);
+		return (0);
+	}
+	//memset(statistic.forks, 0, statistic.nbr_philos * sizeof(pthread_mutex_t));
+	statistic->death = malloc(sizeof(pthread_mutex_t));
+	if (!statistic->death)
+	{
+		free(statistic->forks);
+		free(*philo);
+		return (0);
+	}
+	//memset(statistic.death, 0, statistic.nbr_philos * sizeof(pthread_mutex_t));
+	pthread_mutex_init(statistic->death,NULL);
+	return (1);
+}
+
+void	execute_philo(t_statistics statistic, unsigned int must_eat)
 {
 	t_philosopher	*philo;
 	pthread_t		keeper_thread;
 	unsigned int	i;
 
-	// t_statistics	statistic;
-	// creo os datos
-	// t_philosopher philos;
-	if (statistic.nbr_philos == 0 || statistic.time_die == 0
-		|| statistic.time_eat == 0 || statistic.time_sleep == 0)
-		printf("Os argumetos deberian ser maiores que cero\n");
-	// empezo os filosofos
-	philo = malloc(statistic.nbr_philos * sizeof(t_philosopher));
-	if (!philo)
-	{
-		// liberalo
-		// return (1); // cambialo
-	}
-	//memset(philo, 0, statistic.nbr_philos * sizeof(t_philosopher));
-	statistic.forks = malloc(statistic.nbr_philos * sizeof(pthread_mutex_t));
-	if (!statistic.forks)
-	{
-		// liberalo
-		// return (1); // cambialo
-	}
-	//memset(statistic.forks, 0, statistic.nbr_philos * sizeof(pthread_mutex_t));
-	statistic.death = malloc(sizeof(pthread_mutex_t));
-	if (!statistic.forks)
-	{
-		// liberalo
-		// return (1); // cambialo
-	}
-	//memset(statistic.death, 0, statistic.nbr_philos * sizeof(pthread_mutex_t));
-	pthread_mutex_init(statistic.death, NULL);
-	statistic.nbr_eats = 0; // cambialo
+	if (!init_resources(&statistic, &philo))
+		return ;
+	statistic.nbr_eats = must_eat;
 	start_forks(statistic.nbr_philos, &statistic);
 	statistic.start_time = get_time();
 	start_philo(philo, &statistic);
-	printf("%lu\n", statistic.start_time);
 	start_threads(philo);
 	pthread_create(&keeper_thread, NULL, keeper_function, philo);
 	pthread_join(keeper_thread, NULL);
@@ -61,48 +76,5 @@ void	simple_philo(t_statistics statistic)
 		pthread_join(philo[i].thread, NULL);
 		i++;
 	}
-}
-void	advanced_philo(t_statistics statistic, unsigned int must_eat)
-{
-	t_philosopher	*philo;
-
-	// t_statistics	statistic;
-	// creo os datos
-	if (statistic.nbr_philos == 0 || statistic.time_die == 0
-		|| statistic.time_eat == 0 || statistic.time_sleep == 0)
-		// cinco argumentos podo?
-		printf("Os argumetos deberian ser maiores que cero\n");
-	// statistic.nbr_philos = philos;
-	// statistic.time_die = die; // ten que ser xa en milisegundos ?
-	// statistic.time_eat = eat;
-	// statistic.time_sleep = sleep;
-	statistic.nbr_eats = must_eat;
-	// empezo os filosofos
-	philo = malloc(statistic.nbr_philos * sizeof(t_philosopher));
-	if (!philo)
-	{
-		// liberar todo
-		// return (1); // cambialo
-	}
-	statistic.forks = malloc(statistic.nbr_philos * sizeof(pthread_mutex_t));
-	if (!statistic.forks)
-	{
-		// liberalo
-		// return (1); // cambialo
-	}
-	statistic.death = malloc(sizeof(pthread_mutex_t));
-	if (!statistic.forks)
-	{
-		// liberalo
-		// return (1); // cambialo
-	}
-	pthread_mutex_init(statistic.death, NULL);
-	start_forks(statistic.nbr_philos, &statistic);
-	statistic.start_time = get_time();
-	start_philo(philo, &statistic);
-	statistic.start_time = get_time();
-	printf("%lu\n", statistic.start_time);
-	start_threads(philo);
-	// pthread_create(&keeper_thread, NULL, keeper_function, philo);
-	// pthread_join(keeper_thread,NULL);
+	free_resources(&statistic, philo);
 }
