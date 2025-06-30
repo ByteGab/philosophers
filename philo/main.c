742e1493dc3f9efc9ad2aa4fc8854087e8f9a832
ByteGab <gabrielfreiresimon@gmail.com>
2025-06-30 13:20:02 +0200

    change printf

--- philo/main.c
+++ philo/main.c
@@ -6,7 +6,7 @@
 /*   By: gafreire <gafreire@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/25 12:08:48 by gafreire          #+#    #+#             */
-/*   Updated: 2025/06/28 22:08:49 by gafreire         ###   ########.fr       */
+/*   Updated: 2025/06/30 11:46:49 by gafreire         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,7 +20,7 @@ int	check_params(int argc, char **argv)
 	while (i < argc)
 	{
 		if (ft_atoui(argv[i]) == 0)
-			return (printf("Argumento no valido: %s\n", argv[i]), 0);
+			return (printf("Argument not valid: %s\n", argv[i]), 0);
 		i++;
 	}
 	return (1);
