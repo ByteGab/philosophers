a90265321abf5a0975b2c702d711b812385796aa
ByteGab <gabrielfreiresimon@gmail.com>
2025-06-30 13:20:31 +0200

    delete coment

--- philo/philo.c
+++ philo/philo.c
@@ -6,7 +6,7 @@
 /*   By: gafreire <gafreire@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/03 15:02:43 by gafreire          #+#    #+#             */
-/*   Updated: 2025/06/28 22:14:42 by gafreire         ###   ########.fr       */
+/*   Updated: 2025/06/30 09:25:18 by gafreire         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,10 +14,9 @@
 
 void	free_resources(t_statistics *statistic, t_philosopher *philo)
 {
-	unsigned int i;
+	unsigned int	i;
 
 	i = 0;
-
 	while (i < statistic->nbr_philos)
 	{
 		pthread_mutex_destroy(&statistic->forks[i]);
@@ -27,22 +26,24 @@ void	free_resources(t_statistics *statistic, t_philosopher *philo)
 	free(statistic->forks);
 	free(statistic->death);
 	free(philo);
-	
 }
+/*
+-	memset(philo, 0, statistic.nbr_philos * sizeof(t_philosopher));
+-	memset(statistic.forks, 0, statistic.nbr_philos * sizeof(pthread_mutex_t));
+-   memset(statistic.death, 0, statistic.nbr_philos * sizeof(pthread_mutex_t));
+*/
 
 int	init_resources(t_statistics *statistic, t_philosopher **philo)
 {
 	*philo = malloc(statistic->nbr_philos * sizeof(t_philosopher));
 	if (!*philo)
 		return (0);
-	//memset(philo, 0, statistic.nbr_philos * sizeof(t_philosopher));
 	statistic->forks = malloc(statistic->nbr_philos * sizeof(pthread_mutex_t));
 	if (!statistic->forks)
 	{
 		free(*philo);
 		return (0);
 	}
-	//memset(statistic.forks, 0, statistic.nbr_philos * sizeof(pthread_mutex_t));
 	statistic->death = malloc(sizeof(pthread_mutex_t));
 	if (!statistic->death)
 	{
@@ -50,8 +51,7 @@ int	init_resources(t_statistics *statistic, t_philosopher **philo)
 		free(*philo);
 		return (0);
 	}
-	//memset(statistic.death, 0, statistic.nbr_philos * sizeof(pthread_mutex_t));
-	pthread_mutex_init(statistic->death,NULL);
+	pthread_mutex_init(statistic->death, NULL);
 	return (1);
 }
 
