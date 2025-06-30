4cc94f086eb2509b4804a1c53b7e3abd918602ee
ByteGab <gabrielfreiresimon@gmail.com>
2025-06-30 13:20:43 +0200

    delete coment

--- philo/philosopher.h
+++ philo/philosopher.h
@@ -6,7 +6,7 @@
 /*   By: gafreire <gafreire@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/02 19:38:00 by gafreire          #+#    #+#             */
-/*   Updated: 2025/06/28 22:13:07 by gafreire         ###   ########.fr       */
+/*   Updated: 2025/06/30 13:13:48 by gafreire         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,30 +24,19 @@ typedef struct s_statistics
 {
 	unsigned int	nbr_philos;
 	unsigned int	nbr_eats;
-	// os argumentos
 	unsigned int	time_eat;
 	unsigned int	time_sleep;
 	unsigned int	time_die;
-	// comenzo do tempo
 	unsigned long	start_time;
-	// control do mutex
 	pthread_mutex_t	*forks;
 	pthread_mutex_t	*death;
-	// mirar si morreu os filosofos
 	int				is_dead;
 }					t_statistics;
 
-/*
--	son punteiros porque apuntan aos elementos especificos 
-	do array de forks (chequear)
--	a ultima vez que comeu (en microsegundos)
--	cantas veces comeu
--	punteiro a outra estructura (cambiar o nome)
-*/
 typedef struct s_philosopher
 {
 	int				id;
-	pthread_t		thread;	
+	pthread_t		thread;
 	pthread_mutex_t	*fork_left;
 	pthread_mutex_t	*fork_right;
 	unsigned long	last_eat;
@@ -56,17 +45,22 @@ typedef struct s_philosopher
 	t_statistics	*statistic;
 }					t_philosopher;
 
-void	execute_philo(t_statistics statistic, unsigned int must_eat);
+void				execute_philo(t_statistics statistic,
+						unsigned int must_eat);
 // utils
 unsigned int		ft_atoui(char *str);
 unsigned long		get_time(void);
 // init
-void		start_forks(int nbr_philo, t_statistics *statistics);
-void		start_philo(t_philosopher *philo, t_statistics *statistics);
-void		start_threads(t_philosopher *philo);
+void				start_forks(int nbr_philo, t_statistics *statistics);
+void				start_philo(t_philosopher *philo, t_statistics *statistics);
+void				start_threads(t_philosopher *philo);
 // routine
-void		*routine(void *arg);
-void		*keeper_function(void *arg);
-int			is_philo_dead(t_philosopher *philo);
+void				*routine(void *arg);
+void				*keeper_function(void *arg);
+int					is_philo_dead(t_philosopher *philo);
+
+int					one_philo(t_philosopher *philo);
+void				smart_sleep(unsigned int duration, t_philosopher *philo);
+void				*keeper_function(void *arg);
 
 #endif
