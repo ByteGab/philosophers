f1a7b670798ead701d73aa9d66eae605ead1ae68
ByteGab <gabrielfreiresimon@gmail.com>
2025-07-08 10:38:20 +0200

    delete time sleep

--- philo/utils_routine.c
+++ philo/utils_routine.c
@@ -33,34 +33,44 @@ void	smart_sleep(unsigned int duration, t_philosopher *philo)
 
 	start = get_time();
 	while (!is_philo_dead(philo) && get_time() - start < duration)
-		usleep(100);
+		usleep(50);
 }
 
 void	*keeper_function(void *arg)
 {
-	unsigned int	i;
 	t_philosopher	*philo;
+	unsigned int	i, done;
 
 	philo = (t_philosopher *)arg;
-	while (philo[0].statistic->is_dead != 1)
+	while (1)
 	{
-		usleep(1000);
+		usleep(100);
 		i = 0;
+		done = 0;
 		while (i < philo[0].statistic->nbr_philos)
 		{
 			pthread_mutex_lock(&philo[i].eat_mutex);
-			if (get_time() - philo[i].last_eat > philo[0].statistic->time_die)
+			if (philo->statistic->nbr_eats 
+					&& philo[i].count_eat >= philo->statistic->nbr_eats)
 			{
-				printf("%5ld %2d died 💀\n", (get_time()
-						- philo->statistic->start_time), philo->id);
+				done++;
+				pthread_mutex_unlock(&philo[i].eat_mutex);
+				i++;
+				continue;
+			}
+			if (get_time() - philo[i].last_eat > philo->statistic->time_die)
+			{
+				printf("%5ld %2d died 💀\n", get_time() - philo->statistic->start_time, philo[i].id);
 				pthread_mutex_lock(philo[i].statistic->death);
 				philo[i].statistic->is_dead = 1;
 				pthread_mutex_unlock(philo[i].statistic->death);
+				pthread_mutex_unlock(&philo[i].eat_mutex);
 				return (NULL);
 			}
 			pthread_mutex_unlock(&philo[i].eat_mutex);
 			i++;
 		}
+		if (philo->statistic->nbr_eats && done == philo->statistic->nbr_philos)
+			return (NULL);
 	}
-	return (NULL);
 }
